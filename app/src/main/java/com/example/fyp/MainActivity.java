2fca842b21453e2551fea1da440a4893342fec64
Rob <17232783@studentmail.ul.ie>
2021-02-07 16:09:04 +0000

    Login Added
    Empty Login Activity

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -11,16 +11,21 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
@@ -33,33 +38,34 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     TextView textView;
     Bitmap bitmap;
+    Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Main Activity is running");
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        //Get User Information
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        Log.d("tag","onCreate" +firebaseAuth.getCurrentUser().getEmail() + firebaseAuth.getCurrentUser().getDisplayName());
+        logOutButton =findViewById(R.id.button);
+
         //find imageview
         imageView = findViewById(R.id.imageId);
         //find textview
         textView = findViewById(R.id.textId);
-
         Bitmap bitmap = null;
         String filename = getIntent().getStringExtra("image");
         try {
             FileInputStream is = this.openFileInput(filename);
             bitmap = BitmapFactory.decodeStream(is);
             is.close();
-
-
-            //process the image
-            //1. create a FirebaseVisionImage object from a Bitmap object
+            //1. create a FirebaseVisionImage object from a Bitmap
             FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-            //2. Get an instance of FirebaseVision
+            //2. Get an instance of FirebaseVision using a cloudTextRecognizer
             FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
                     .getCloudTextRecognizer();
-            //3. Create an instance of FirebaseVisionTextRecognizer
-            //4. Create a task to process the image
+            //3. Create a task to process the image
             Task<FirebaseVisionText> result =
                     detector.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                         @Override
@@ -102,12 +108,12 @@ public class MainActivity extends AppCompatActivity {
         //open the camera => create an Intent object
         System.out.println("Button Clicked");
         System.out.println("Do Process runs");
+        Log.d("tag","Camera is Pressed");
+
 
         Intent cameraActivityIntent = new Intent(getApplicationContext(), CameraActivity.class);
         startActivity(cameraActivityIntent);
     }
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -116,8 +122,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+//Logging the user out
+public void logout(final View view) {
+    FirebaseAuth.getInstance().signOut();
+    Log.d("tag","LogoutButton is Pressed");
 
-    public void OCR(View view) {
-
-    }
+    GoogleSignIn.getClient(this,new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build())
+            .signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+        @Override
+        public void onSuccess(Void aVoid) {
+            startActivity(new Intent(view.getContext(),LoginActivity.class));
+        }
+    }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            Toast.makeText(MainActivity.this, "Signout Failed.", Toast.LENGTH_SHORT).show();
+        }
+    });
+}
 }
